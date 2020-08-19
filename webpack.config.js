f9f70f5a157b03e49c912fad7798a359300fa214
Tom-Rune Bornholdt <trb@braune-digital.com>
2020-08-19 17:11:02 +0200

    feat: Add file loader for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -40,12 +40,12 @@ module.exports = (env, argv) => {
           ],
         },
         {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-            },
-          ],
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: ['file-loader'],
         },
       ],
     },
